8562a8395922308829c7f78a8d4c3326a8bf950e
Julio Valdez <valdezjulio95@gmail.com>
2021-10-06 02:40:05 -0500

    added initial menu category images

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const btnEl = document.querySelector('button')
 const output = document.getElementById('output').childNodes[0]
 
-btnEl.addEventListener('click', function () {
-  const inputVal = document.querySelector('input').value
-  output.innerText = inputVal.toUpperCase()
+const inputEl = document.querySelector('input')
+inputEl.addEventListener('keyup', function (e) {
+  output.innerText = inputEl.value.toUpperCase()
 })
